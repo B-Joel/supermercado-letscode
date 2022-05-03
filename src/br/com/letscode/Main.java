b70203286b4ecaa0200bf5850cd59e5226a57240
B-Joel <joelbellatto@hotmail.com>
2022-05-03 11:35:34 -0300

    test

--- src/br/com/letscode/Main.java
+++ src/br/com/letscode/Main.java
@@ -1,5 +1,18 @@
 package br.com.letscode;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.printf("menu");
+            sc.next();
+
+        }while (true);
+
+    }
 
 }
